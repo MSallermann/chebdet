ce646cfb741aac70d7b59a1b49b820377d2db156
Moritz Sallermann <moritzsallermann@gmail.com>
2024-08-15 17:41:14 +0000

    tests: test for general pd matrices

---
+++ test/test_deflation_sigma_max.py
@@ -0,0 +1,43 @@
+import numpy as np
+import util
+from chebdet import chebdet
+import scipy as sp
+
+
+def test():
+    n = int(1e2)
+
+    print(f"Testing a {n} by {n} matrix")
+
+    lambda_min = 1
+    lambda_max = 10
+
+    n_sample = 100
+    n_degree = 100
+
+    # define deflation vectors
+    eigenvalues_deflate = [-10.0, 0.0]
+    n_deflate = len(eigenvalues_deflate)
+    eigenvectors_deflate = [util.standard_basis_vector(i, n) for i in range(n_deflate)]
+
+    # Test with deflation
+    diag = np.linspace(lambda_min, lambda_max, n)
+    diag[:n_deflate] = eigenvalues_deflate
+
+    B = sp.sparse.diags(diag)
+
+    logdet = np.sum(np.log(diag[n_deflate:]))
+
+    logdet_algorithm = chebdet.log_det_positive_definite(
+        matrix=B,
+        n_sample=n_sample,
+        n_degree=n_degree,
+        sigma_max=lambda_max * 1.1,
+        eigenvalues_deflate=eigenvalues_deflate,
+        eigenvectors_deflate=eigenvectors_deflate,
+    )
+
+    print(f"{logdet = }")
+    print(f"{logdet_algorithm = }")
+
+    assert np.isclose(logdet, logdet_algorithm)
---
+++ test/test_random_dense_sigma_max.py
@@ -0,0 +1,34 @@
+from chebdet import chebdet
+import numpy as np
+import scipy as sp
+from numpy.typing import NDArray
+import util
+
+
+def test():
+    n = 20
+
+    print(f"Testing a {n} by {n} matrix")
+
+    lambda_min = 1
+    lambda_max = 9
+    # delta = 0.01
+
+    n_sample = 120
+    n_degree = 80
+
+    diag = np.linspace(lambda_min, lambda_max, n)
+
+    # B = util.generate_matrix(spectrum=diag)
+    B = sp.sparse.diags(diag)
+
+    logdet = np.sum(np.log(diag))
+
+    logdet_algorithm = chebdet.log_det_positive_definite(
+        matrix=B, n_sample=n_sample, n_degree=n_degree, sigma_max=lambda_max * 1.1
+    )
+
+    print(f"{logdet = }")
+    print(f"{logdet_algorithm = }")
+
+    assert np.isclose(logdet, logdet_algorithm)
