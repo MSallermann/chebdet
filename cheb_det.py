1153969e8faa81f05b90153061cee00edb344633
Moritz Sallermann <moritzsallermann@gmail.com>
2024-08-15 16:28:04 +0000

    cheb_det: nicer typing

--- cheb_det.py
+++ cheb_det.py
@@ -1,14 +1,11 @@
-from scipy.sparse import eye
-from scipy.sparse.linalg import splu
-
 import scipy.sparse
-from scipy.sparse import coo_array, csr_array, eye
+from scipy.sparse import csr_array
 import numpy as np
 from numpy.typing import NDArray
-from typing import Optional, List
+from typing import Optional, List, Union
 
 
-def Rademacher_vec(len):
+def Rademacher_vec(len: int) -> NDArray[np.float64]:
     return 2.0 * np.random.randint(2, size=(len, 1)) - 1.0
 
 
@@ -47,7 +44,7 @@ def chebyshev_coeffs(delta: float, n_degree: int) -> NDArray[np.float64]:
 
 
 def logdet(
-    matrix: csr_array,
+    matrix: Union[csr_array, NDArray],
     n_sample: int,
     n_degree: int,
     delta: float,
@@ -57,12 +54,12 @@ def logdet(
     """Computes the log-determinant using the chebyshev approximation
 
     Args:
-        matrix (csr_array): positive definite determinant
+        matrix (csr_array | NDArray): positive definite determinant
         n_sample (int): number of random samples
         n_degree (int): polynomial degree of chebyshev approximation
         delta (float): the eigenvalues of matrix have to be in the interval [delta, 1-delta]
-        eigenvalues_deflate (Optional[List[float]]): _description_
-        eigenvectors_deflate (Optional[List[NDArray[np.float64]]]): _description_
+        eigenvalues_deflate (Optional[List[float]]): The list of eigenvalues to remove
+        eigenvectors_deflate (Optional[List[NDArray[np.float64]]]): The list of eigenvectors to remove
 
 
     Returns:
@@ -72,7 +69,9 @@ def logdet(
     deflate = not eigenvalues_deflate is None and not eigenvectors_deflate is None
     if deflate:
         if len(eigenvalues_deflate) != len(eigenvectors_deflate):
-            raise Exception("Eigenvectors deflate and eigenvlaues deflate need to have the same length")
+            raise Exception(
+                "Eigenvectors deflate and eigenvlaues deflate need to have the same length"
+            )
 
         # For the deflation use a target that's within the [delta, 1-delta] interval
         deflate_target = 0.5
@@ -82,7 +81,7 @@ def logdet(
     I = scipy.sparse.eye(d, d)
 
     scale = 2.0 / (2.0 * delta - 1.0)
-    shift = -scale/2.0
+    shift = -scale / 2.0
 
     A = scale * matrix + shift * I
 
@@ -90,12 +89,12 @@ def logdet(
 
     c = chebyshev_coeffs(delta, n_degree)
 
-    def mult_A(lhs : NDArray[np.float64]):
-        res = A@lhs
+    def mult_A(lhs: NDArray[np.float64]):
+        res = A @ lhs
 
         if deflate:
-            for eval,evec in zip(eigenvalues_deflate,eigenvectors_deflate):
-                res += scale * (deflate_target - eval) * evec * (evec.T@lhs)[0,0]
+            for eval, evec in zip(eigenvalues_deflate, eigenvectors_deflate):
+                res += scale * (deflate_target - eval) * evec * (evec.T @ lhs)[0, 0]
 
         return res
 
@@ -114,7 +113,7 @@ def logdet(
                 w1 = w2
         Gamma = Gamma + v.T @ u / n_sample
 
-    res = Gamma[0,0]
+    res = Gamma[0, 0]
 
     if deflate:
         for i in range(n_deflate):
